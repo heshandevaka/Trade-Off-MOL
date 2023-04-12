f2d24ab19c2efe8fb6a470e041041b2cc2e194b1
heshandevaka <heshan.devaka@gmail.com>
2023-04-12 18:08:45 -0400

    update run file

--- LibMTL/examples/office/run.sh
+++ LibMTL/examples/office/run.sh
@@ -1,1 +1,4 @@
-python -u train_office.py --multi_input --dataset_path office-31 --weighting MoCo --rho_moco 0.5 --gpu_id 0 2>&1 >> train_logs/moco_rho_0.5_test.out &
+# MoCo
+# python -u train_office.py --multi_input --dataset_path office-31 --weighting MoCo --rho_moco 0.5 --gpu_id 0 2>&1 >> train_logs/moco_rho_0.5_test.out &
+# EW
+python -u train_office.py --multi_input --dataset_path office-31 --gpu_id 0 2>&1 >> train_logs/ew_init.out &
